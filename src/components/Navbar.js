03d05701bfa7d072e397c8684a893daef8b9cb2d
danny <dli0906@gmail.com>
2019-09-14 19:07:21 -0400

    styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import './styles.css';
 
 
 export default function Navagation() {
   return (
-    <Navbar bg="light" expand='lg'>
+    <Navbar class='navbar' bg='light' expand='lg'>
       <Navbar.Brand href='#home'>Danny Li</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav>
+        <Nav class='nav'>
           <Nav.Link href='#home'>Home</Nav.Link>
           <Nav.Link href='#about'>About</Nav.Link>
           <Nav.Link href='#projects'>Projects</Nav.Link>
