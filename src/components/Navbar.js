a98339267c959234beac31046a3f7c8d5612dcfb
danny <dli0906@gmail.com>
2019-09-14 18:37:23 -0400

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
+export default function Navagation() {
+  return (
+    <Navbar bg="light" expand='lg'>
+      <Navbar.Brand href='#home'>Danny Li</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav>
+          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href='#about'>About</Nav.Link>
+          <Nav.Link href='#projects'>Projects</Nav.Link>
+          <Nav.Link href='#blog'>Blog</Nav.Link>
+          <Nav.Link href='#contact'>Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
