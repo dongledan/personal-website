16250d12a34df42899822c8c4f647207bd419b9b
danny <dli0906@gmail.com>
2019-10-11 23:23:18 -0400

    added photos of griffin

--- src/components/photos/arizona.js
+++ src/components/photos/arizona.js
@@ -1,12 +1,12 @@
 const arizona = [
   'https://i.imgur.com/3PaQ60l.jpg',
   'https://i.imgur.com/9HtjUAH.jpg',
-  'https://i.imgur.com/i1xOLJh.jpg',
+  'https://i.imgur.com/Rc6WrWn.jpg',
   'https://i.imgur.com/oCGgNv4.jpg',
   'https://i.imgur.com/9qf7DZQ.jpg',
   'https://i.imgur.com/XBx01eI.jpg',
   'https://i.imgur.com/jD6LIfJ.jpg',
-  'https://i.imgur.com/4o8HtE6.jpg',
+  'https://i.imgur.com/XA3OFr2.jpg',
 
 
 ]
