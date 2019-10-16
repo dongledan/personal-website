95b8a5d053adfa6014d6d08631559730801582d2
danny <dli0906@gmail.com>
2019-10-16 18:30:14 -0400

    revert

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,8 @@ import Image from 'react-bootstrap/Image';
 
 export default function Title() {
   return (
-    <div class='home container-fluid'>
-      {/* <Image src='https://i.imgur.com/sJH7vkz.jpg' fluid data-aos='fade-down' /> */}
+    <div class='home'>
+      <Image src='https://i.imgur.com/sJH7vkz.jpg' fluid data-aos='fade-down' />
       <div class='container-header'>
         <h3 class='lead header-text'>Welcome to my site!</h3>
         <h3 class='lead header-text-2'>Danny Li</h3>
