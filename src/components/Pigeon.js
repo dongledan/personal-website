824e15a58bb4893b0762c0ff8fe3909ec9b58361
danny <dli0906@gmail.com>
2019-10-15 22:23:58 -0400

    margin in some spots

--- src/components/Pigeon.js
+++ src/components/Pigeon.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import './Pigeon.css';
 
 export default function Pigeon() {
   return (
-    <div className='pigeon-bg'>
+    <div data-aos='fade'>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
