07487fcfbe0f617c20ff249d536451bdc71b69d4
danny <dli0906@gmail.com>
2020-03-20 16:12:55 -0400

    link to project 3

--- src/components/variables/projects.js
+++ src/components/variables/projects.js
@@ -35,7 +35,7 @@ const projects = [
     name: "LBs to KG",
     src: "assets/lbsToKg.png",
     desc: `This is a personal app that I made for myself after competing in a USAPL meet. Leading up to the meet, I trained using pound plates and did not have exposure to kilo plates. I cannot do quick maths when decimals are involved. Using react native, expo, node.js, and plain javascript-- now I don't have to do quick maths! By placing in a weight in lbs, the app will tell you how many plates you need with a helpful graphic for visualization!`,
-    href: "TBD",
+    href: "https://expo.io/@dongledan/lbs-to-kg",
     footer: `React Native, Node.js, Expo`
   }
 ];
